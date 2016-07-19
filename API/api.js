a52a820c4125d067a3688a024232566b70f203e2
Daniel Ashcraft <ashcraftbookkeeping@gmail.com>
2016-07-19 17:23:03 +0000

    Basic server up and running, seperated API from routes

---
+++ API/api.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var api_routes =  require('./API/api')
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -15,20 +17,13 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+
+app.use('/api', api_routes);
 
 // START THE SERVER
 // =============================================================================

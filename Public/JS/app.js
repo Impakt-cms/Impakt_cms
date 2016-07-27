6fb17f0c306278d925d19567f6f7d59c98bbe3c7
Omid Nowrouzi <onowrouzi@uco.edu>
2016-07-27 17:55:27 -0500

    Added initialization of cookieStore auth to prevent lack of loading login.

--- Public/JS/app.js
+++ Public/JS/app.js
@@ -4,8 +4,9 @@
 //this is where the home-route will go
 angular.module('app', ['ui.router', 'ui.bootstrap', 'angularMoment', 'ngCookies', 'ngFileUpload'])
 	//declare authentication global variable.
-	.run(function($rootScope) {
+	.run(function($rootScope, $cookieStore) {
 		$rootScope.auth = false;
+		$cookieStore.put('auth', false);
 	});
 	
 	//Routes defined in router.js

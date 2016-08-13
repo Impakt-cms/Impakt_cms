42dd515bd523b4f1dc0858ac94b14352f132cb07
Jason Carpenter <jason9199@gmail.com>
2016-08-12 23:03:23 -0500

    reloaded modules and updated gulp tasks so you can run npm test and it will watch js files for changes, re-build them on change, and automatically load the web browser to launch the app

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
-var gulp       = require('gulp')
-var concat     = require('gulp-concat')
-var ngAnnotate = require('gulp-ng-annotate');
-var uglify     = require('gulp-uglify');
+var gulp        = require('gulp');
+var concat      = require('gulp-concat');
+var ngAnnotate  = require('gulp-ng-annotate');
+var uglify      = require('gulp-uglify');
+var nodemon     = require('gulp-nodemon');
+var browserSync = require('browser-sync');
 
 gulp.task('build', function () {
-  gulp.src(['public/admin/js/app.js',
-  	'!public/admin/js/bundle.js',
-	'public/admin/**/*.js'])
-    .pipe(ngAnnotate())
-    .pipe(concat('bundle.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/admin/js'))
+	gulp.src(['public/admin/js/app.js',
+		'!public/admin/js/bundle.js',
+		'public/admin/**/*.js'])
+		.pipe(ngAnnotate())
+		.pipe(concat('bundle.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('public/admin/js'))
 })
 
-gulp.task('watch', function () {
-  gulp.watch('public/admin/**/*.js', ['build'])
+gulp.task('start', function () {
+	nodemon({
+		script: 'server.js'
+		, ext: 'js html'
+		, env: { 'NODE_ENV': 'development' }
+	})
+})
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		proxy: "localhost:8080"
+    });
+})
+
+gulp.task('watch', ['start', 'browser-sync'], function () {
+	gulp.watch('public/admin/**/*.js', ['build'])
 })

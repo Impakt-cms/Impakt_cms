c183696553c020451734fe09d752eae7b0481e0c
onowrouzi <onowrouzi@outlook.com>
2016-08-15 23:36:59 -0500

    Reimplemented $rootScope auth variable for navbar show/hide elements.

--- public/admin/js/app.js
+++ public/admin/js/app.js
@@ -8,7 +8,11 @@
 		'angularMoment',
 		'ngCookies',
 		'ngFileUpload'
-	]);
+	])
+	//Get scope variable based of logged in status.
+	.run(function($rootScope, $cookieStore){
+		$rootScope.auth = $cookieStore.get('auth');
+	});
 	
 	//View routes defined in router.js
 })();

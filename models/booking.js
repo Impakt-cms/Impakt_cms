c01271aadff5be6780243294a9beafa4d503d783
Daniel Ashcraft <ashcraftbookkeeping@gmail.com>
2016-08-17 16:46:12 +0000

    Added booking route and model

---
+++ models/booking.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var bookingSchema = new Schema({
+  bookingSubmitter: {type:String,required:true},
+  Email:{type:String, required:true},
+  submittedDate: Date,
+  StartDate: Date,
+  EndDate: Date,
+  Time: String,
+  Approved:{type:Boolean, default:false},
+  ApprovedBy: Schema.Types.ObjectId
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Booking = mongoose.model('Booking', bookingSchema);
+
+// make this available to our users in our Node applications
+module.exports = Booking;

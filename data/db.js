9aee3ae040e07b7229e90571e3e16bd5db61b5ba
dashcraft <ashcraftbookkeeping@gmail.com>
2016-08-28 19:40:56 -0500

    Add track by

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var uri = process.env.MONGOLAB_URI;
+var uri = 'mongodb://heroku_1vpzkhfd:qbcgcvaid9rp64vtl2k2s5se4s@ds017736.mlab.com:17736/heroku_1vpzkhfd';
 
 var db = mongoose.connect(uri,function(){
 	console.log('Db has been connected!')

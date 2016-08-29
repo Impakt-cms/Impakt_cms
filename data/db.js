c4c17419e31457e346789a8fc9ae4caba3b13cd1
dashcraft <ashcraftbookkeeping@gmail.com>
2016-08-28 19:48:29 -0500

    Commit all the things

--- data/db.js
+++ data/db.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var uri = 'mongodb://heroku_1vpzkhfd:qbcgcvaid9rp64vtl2k2s5se4s@ds017736.mlab.com:17736/heroku_1vpzkhfd';
+var uri = process.env.MONGOLAB_URI || 'localhost:27017/Impakt';
+mongoose.connect(uri, function(){
 
+	console.log('success');
+
+})
 var db = mongoose.connect(uri,function(){
 	console.log('Db has been connected!')
 });

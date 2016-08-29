ff511e4b25be654f872d66dd6d4920c731e3c644
dashcraft <ashcraftbookkeeping@gmail.com>
2016-08-28 19:31:58 -0500

    Add track by

--- data/db.js
+++ data/db.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var uri = process.env.MONGOLAB_URI ||
-		  process.env.MONGOHQ_URL || 
-		  'mongodb://localhost/Impakt';
+var uri = process.env.MONGOLAB_URI;
 
 var db = mongoose.connect(uri,function(){
 	console.log('Db has been connected!')

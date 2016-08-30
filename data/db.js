0c70a423bbc7b8c68d1a029325aa4b218145becb
Omid Nowrouzi <onowrouzi@uco.edu>
2016-08-29 21:53:26 -0500

    Using full mongolab url.

--- data/db.js
+++ data/db.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var uri = process.env.MONGOLAB_URI ||
-		  process.env.MONGOHQ_URL || 
+		  process.env.MONGOHQ_URL ||
 		  'mongodb://localhost/Impakt';
 
 
-var db = mongoose.connect(uri,function(){
-	console.log(uri);
-	console.log('Db has been connected!')
+var db = mongoose.connect(//uri,function(){
+	'mongodb://heroku_1vpzkhfd:qbcgcvaid9rp64vtl2k2s5se4s@ds017736.mlab.com:17736/heroku_1vpzkhfd', function(){
+		console.log(uri);
+		console.log('Db has been connected!')
 });
 
 module.exports = db;

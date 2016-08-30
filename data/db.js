258116345d5d7f65aa7b8589ac9a926395bb3c9f
Daniel Ashcraft <ashcraftbookkeeping@gmail.com>
2016-08-29 21:23:07 -0500

    Update db.js

--- data/db.js
+++ data/db.js
@@ -1,12 +1,9 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var uri = process.env.MONGOLAB_URI || 'localhost:27017/Impakt';
-mongoose.connect(uri, function(){
 
-	console.log('success');
-
-})
 var db = mongoose.connect(uri,function(){
+	console.log(uri);
 	console.log('Db has been connected!')
 });
 

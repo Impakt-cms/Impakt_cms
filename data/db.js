507db7baea070f6d3c6fa2125c08ae78dfeada33
dashcraft <ashcraftbookkeeping@gmail.com>
2016-09-06 14:43:41 -0500

    Add partial

--- data/db.js
+++ data/db.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 var uri = process.env.MONGOLAB_URI ||
 		  process.env.MONGOHQ_URL ||
 		  'mongodb://localhost/Impakt';
-
+		  
 var db = mongoose.connect(//uri,function(){
 	'mongodb://heroku_1vpzkhfd:qbcgcvaid9rp64vtl2k2s5se4s@ds017736.mlab.com:17736/heroku_1vpzkhfd', function(){
 		console.log(uri);
